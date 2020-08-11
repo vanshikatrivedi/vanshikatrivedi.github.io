3f66c78bf555986a7b702021d0dfe6af52875479
vanshikatrivedi <vanshika13574@gmail.com>
2020-08-12 01:23:53 +0530

    format update

--- scripts/script.js
+++ scripts/script.js
@@ -1,34 +1,32 @@
-
 //button to toggle the theme
 
-let body = document.querySelector('body');
-      function toggleTheme() {
-        if (body.className == 'default') {
-          body.className = 'dark';
-        } else {
-          body.className = 'default';
-        }
-      }
+let body = document.querySelector("body");
+function toggleTheme() {
+  if (body.className == "default") {
+    body.className = "dark";
+  } else {
+    body.className = "default";
+  }
+}
 
 //navigation functions
 
-let nav = document.querySelector('.nav');
+let nav = document.querySelector(".nav");
 let menuOpen = false;
-function toggleNav(){
-  if (!menuOpen){
-    nav.style.transform = `translateX(-100%)`
+function toggleNav() {
+  if (!menuOpen) {
+    nav.style.transform = `translateX(-100%)`;
     menuOpen = !menuOpen;
-  }
-  else{
-    nav.style.transform = `translateX(0)`
+  } else {
+    nav.style.transform = `translateX(0)`;
     menuOpen = !menuOpen;
   }
 }
 
+// 
 
-
-
-    
-      
-
-      
+// let typed = new Typed('.section__subtitle--intro', {
+//   strings: ['a coder girl :) ^1000'],
+//   smartBackspace: true, 
+//   typeSpeed: 40
+// });
