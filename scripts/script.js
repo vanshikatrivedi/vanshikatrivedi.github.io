096ac8a8105991176f51fe9e9526a5323f2504ef
vanshikatrivedi <vanshika13574@gmail.com>
2020-08-13 00:43:12 +0530

    typed addition

--- scripts/script.js
+++ scripts/script.js
@@ -23,10 +23,14 @@ function toggleNav() {
   }
 }
 
-// 
 
-// let typed = new Typed('.section__subtitle--intro', {
-//   strings: ['a coder girl :) ^1000'],
-//   smartBackspace: true, 
-//   typeSpeed: 40
-// });
+
+let typed = new Typed('.section__subtitle--intro p', {
+  strings: ['a coder girl :) ^1000', 'front-end dev ^1000'],
+  smartBackspace: false, 
+  typeSpeed: 40,
+  backSpeed: 30,
+  cursorChar: '',
+  shuffle: true,
+  loop: true,
+});
